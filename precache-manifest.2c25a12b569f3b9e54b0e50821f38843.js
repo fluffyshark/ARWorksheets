244d38b7892bc5a772346155a6cd42c96c976fa8
Robin Yjord <robinyjord@Robins-MacBook-Pro-2.local>
2020-06-21 12:32:29 +0200

    Updates

---
+++ precache-manifest.2c25a12b569f3b9e54b0e50821f38843.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ac7953a3f2e27bb64a088faf039460",
+    "url": "/ARWorksheets/index.html"
+  },
+  {
+    "revision": "e721bc1ea97dc82de232",
+    "url": "/ARWorksheets/static/css/main.71550e4b.chunk.css"
+  },
+  {
+    "revision": "3bcb984cbe173de0b174",
+    "url": "/ARWorksheets/static/js/2.02eeef3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ARWorksheets/static/js/2.02eeef3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e721bc1ea97dc82de232",
+    "url": "/ARWorksheets/static/js/main.1e93ee95.chunk.js"
+  },
+  {
+    "revision": "478d5b3aa7195614d718",
+    "url": "/ARWorksheets/static/js/runtime-main.092364a7.js"
+  },
+  {
+    "revision": "0da62eb9354a0bea226705f9d6e5ec3e",
+    "url": "/ARWorksheets/static/media/background_section.0da62eb9.png"
+  },
+  {
+    "revision": "b59143672118fcad749365bdb2c0781d",
+    "url": "/ARWorksheets/static/media/bigger_background.b5914367.png"
+  },
+  {
+    "revision": "8c2d9c198547b89dfaca897a1a139371",
+    "url": "/ARWorksheets/static/media/contact_img1.8c2d9c19.png"
+  },
+  {
+    "revision": "93298fe69ca51417babba8a6baaafed6",
+    "url": "/ARWorksheets/static/media/crossword01.93298fe6.png"
+  },
+  {
+    "revision": "6b30085a5cd35be7d681abcef3fe9316",
+    "url": "/ARWorksheets/static/media/crossword02.6b30085a.png"
+  },
+  {
+    "revision": "a651abcf68f5e149b34ee51e36f78c44",
+    "url": "/ARWorksheets/static/media/crossword03.a651abcf.png"
+  },
+  {
+    "revision": "f73416079909b31f0d29d9587663f69f",
+    "url": "/ARWorksheets/static/media/crossword04.f7341607.png"
+  },
+  {
+    "revision": "4fe23caa2dd95cc0d9e020ff7ef90a6b",
+    "url": "/ARWorksheets/static/media/download_app_store.4fe23caa.png"
+  },
+  {
+    "revision": "719199657100120883c6cc236423eda6",
+    "url": "/ARWorksheets/static/media/facebook-share.71919965.png"
+  },
+  {
+    "revision": "9c2a83b520643ba01c433771a92564a8",
+    "url": "/ARWorksheets/static/media/iPad_product.9c2a83b5.png"
+  },
+  {
+    "revision": "7aa9ed33c71c72119f6deff620b90a91",
+    "url": "/ARWorksheets/static/media/marine01.7aa9ed33.png"
+  },
+  {
+    "revision": "0ae8f60d7cb2f4ca213131d729271ab8",
+    "url": "/ARWorksheets/static/media/marine02.0ae8f60d.png"
+  },
+  {
+    "revision": "b7876bd541ac50ab15c32375b34bc33d",
+    "url": "/ARWorksheets/static/media/marine03.b7876bd5.png"
+  },
+  {
+    "revision": "c0f4ba6aec0f7c8aaeb7196e482767e0",
+    "url": "/ARWorksheets/static/media/marine04.c0f4ba6a.png"
+  },
+  {
+    "revision": "e2aadb6a5cab26813d6aa4d5d6313352",
+    "url": "/ARWorksheets/static/media/marine05.e2aadb6a.png"
+  },
+  {
+    "revision": "9db1131b1dfa5d189f8aab562d49979c",
+    "url": "/ARWorksheets/static/media/saves_time_icon.9db1131b.png"
+  },
+  {
+    "revision": "475deccd538db351fd0098f697992a71",
+    "url": "/ARWorksheets/static/media/wordpuzzle01.475deccd.png"
+  },
+  {
+    "revision": "c80e423e876b4e4606c9594e2563337f",
+    "url": "/ARWorksheets/static/media/wordpuzzle02.c80e423e.png"
+  },
+  {
+    "revision": "81f401ce5407d0de33c7ca7f5bcf28ba",
+    "url": "/ARWorksheets/static/media/wordpuzzle03.81f401ce.png"
+  },
+  {
+    "revision": "6d0fdc535d5afe7418e416dbf5805d1a",
+    "url": "/ARWorksheets/static/media/wordpuzzle04.6d0fdc53.png"
+  },
+  {
+    "revision": "0527523ddb10df6cf8ff1d4efd4c907b",
+    "url": "/ARWorksheets/static/media/wordpuzzle05.0527523d.png"
+  },
+  {
+    "revision": "e93c5908fdf1c64d4db0839c61901b39",
+    "url": "/ARWorksheets/static/media/worksheet01.e93c5908.png"
+  },
+  {
+    "revision": "c46891119bbecc69e2e6a7e27f691149",
+    "url": "/ARWorksheets/static/media/worksheet02.c4689111.png"
+  },
+  {
+    "revision": "1643659df34bf4e9e50600858b3dac5d",
+    "url": "/ARWorksheets/static/media/worksheet03.1643659d.png"
+  },
+  {
+    "revision": "339ab44f6e3b972c50131c4767fc0852",
+    "url": "/ARWorksheets/static/media/worksheet04.339ab44f.png"
+  },
+  {
+    "revision": "505839630c99b54f008b41e58484e96f",
+    "url": "/ARWorksheets/static/media/worksheet05.50583963.png"
+  }
+]);
